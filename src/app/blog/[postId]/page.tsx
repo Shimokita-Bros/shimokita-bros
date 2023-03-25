c78a85e2d840f61b3845a8680cd5ba3d1bcd58b4
shimokita-bros <tomoyaokuda.info@gmail.com>
2023-03-26 01:38:01 +0900

    ♻️ 環境構築

---
+++ src/app/blog/[postId]/page.tsx
@@ -0,0 +1,38 @@
+import { notFound } from 'next/navigation';
+import parse from 'html-react-parser';
+import { getDetail, getList } from 'libs/microcms';
+import styles from 'src/app/blog/[postId]/post.module.scss';
+
+// キャッシュを利用しない
+export const revalidate = 0;
+
+export async function generateStaticParams() {
+  const { contents } = await getList();
+
+  const paths = contents.map((post) => {
+    return {
+      postId: post.id,
+    };
+  });
+
+  return [...paths];
+}
+
+export default async function StaticDetailPage({
+  params: { postId },
+}: {
+  params: { postId: string };
+}) {
+  const post = await getDetail(postId);
+
+  if (!post) {
+    notFound();
+  }
+
+  return (
+    <main className={styles.main}>
+      <h1 className={styles.title}>{post.title}</h1>
+      <div className={styles.post}>{parse(post.content)}</div>
+    </main>
+  );
+}
